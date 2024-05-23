7c86f93e42ea8e7a0a0be945156f5904a7a26c8a
hertg <3533648+hertg@users.noreply.github.com>
2024-05-23 10:42:08 +0200

    add readme, and script

---
+++ README.md
@@ -0,0 +1,98 @@
+# twirl-intellij-fix
+
+Sometimes IntelliJ messes up your Twirl templates, especially if you are using
+dependency injection. JetBrains is [aware of this issue](https://youtrack.jetbrains.com/issue/SCL-21757/Twirl-Templates-Will-they-ever-be-supported-again)
+for a while, but they still haven't addressed it. I have lost so much time 
+manually fixing my twirl views that it granted the investment to create this
+script.
+
+
+## Usage
+
+```txt
+usage: fix_twirl_scripts.py [-h] [--dry] [file]
+
+Find and fix twirl templates that were messed up by
+IntelliJ refactoring and code format. Run with '--dry'
+flag first to see changes.
+
+positional arguments:
+  file        file or directory, defaults to current working dir
+
+options:
+  -h, --help  show this help message and exit
+  --dry       do not write changes, only output diff
+```
+
+> [!CAUTION]
+> Please note that although this script was heavily tested, it might not always
+> work and could still mess up some things. Run with the `--dry` flag first
+> to see if does what you expect.
+
+> [!NOTE]
+> I'd advise to save the output of this command (diff of made changes) to a 
+> file, so that it can later be analyzed if the script made wrong changes.
+
+
+## What it does
+
+This scripts is straightforward, I advise you to take
+a quick look at it before running it on your computer.
+
+It will recursively search for `.scala.html` files in your working directory,
+or the directory you provided as a parameter. If you provided a file path, 
+the script will only run on the provided file.
+
+To fix a twirl view it will:
+
+- find imports (`@import`) anywhere in the file, even if the have multiple `@@`
+symbols, are at the end of the file, or multiple imports are on the same line
+
+- find the injector (`@this()`) anywhere in the file, even if it has multiple 
+`@@` symbols
+
+- find the view parameters (`@()`) anywhere in the file, even if it has multiple
+`@@` symbols
+
+Then it will reconstruct the view by: 
+
+- fixing all found imports and adding them at the beginning of the file
+- add a linebreak
+- fixing the injector (if found), and add it to the file
+- fixing the view parameters, and add it to the file
+- add a linebreak
+- add the rest of the content (with all previously found imports, injector, 
+params removed) and stripping empty lines at the beginning
+
+
+## Example
+
+An example of a twirl messed up by IntelliJ might look as follows:
+
+```twirl
+@import org.example.Foo
+@@import org.example.Bar
+
+
+@this(bar: Bar)
+@import org.example.Example
+@@(foo: String)(args: (Symbol, Any)*)(implicit bar: String)
+
+<p>Hello @foo</p>@import org.example.Baz
+
+```
+
+The script will fix this to:
+
+```twirl
+@import org.example.Foo
+@import org.example.Bar
+@import org.example.Example
+@import org.example.Baz
+
+@this(bar: Bar)
+@(foo: String)(args: (Symbol, Any)*)(implicit bar: String)
+
+<p>Hello @foo</p>
+
+```
---
+++ fix_twirl_scripts.py
@@ -0,0 +1,139 @@
+import re
+import glob
+import os
+import sys
+import difflib
+import argparse
+import textwrap
+
+
+def isBlank(str):
+    return not (str and str.strip())
+
+
+def fix(lines, filepath):
+    imports = []
+    injection = ''
+    param = ''
+    rest = []
+    out = []
+
+    for line in lines:
+        # imports
+        pattern = re.compile(r'(@+import\s+([^\n@]*))')
+        matches = pattern.findall(line)
+        for match in matches:
+            imports.append('@import ' + match[1] + '\n')
+            line = line.replace(match[0], '')
+            if isBlank(line):
+                continue
+
+        # injector
+        pattern = re.compile(r'(@+this\(([^\n@]*)\))')
+        match = pattern.match(line)
+        if match:
+            injection = '@this(' + match.group(2) + ')\n'
+            line = line.replace(match.group(1), '')
+            if isBlank(line):
+                continue
+
+        # view parameters
+        pattern = re.compile(r'^(@+(\([^\n@]*\)))')
+        match = pattern.match(line)
+        if match:
+            param = '@' + match.group(2) + '\n'
+            line = line.replace(match.group(1), '')
+            if isBlank(line):
+                continue
+
+        rest.append(line)
+
+    if imports:
+        for imp in imports:
+            out.append(imp)
+
+        # add a blank line after imports
+        out.append('\n')
+
+    if injection:
+        out.append(injection)
+
+    if not param:
+        raise Exception('this twirl view "' + filepath + '" has no view parameters?')
+
+    out.append(param)
+
+    # add a blank line after view parameters
+    out.append('\n')
+
+    # all blank lines at the beginning are skipped,
+    # until a non-blank line is encountered
+    skipping_mode = True
+    for line in rest:
+        skipping_mode &= isBlank(line)
+        if not skipping_mode:
+            out.append(line)
+
+    return out
+
+
+def diff(orig, new, filepath):
+    # prevent bug from difflib when the last line doesn't end with a newline
+    # character: https://github.com/pixee/codemodder-python/pull/116
+    diff_lines = list(difflib.unified_diff(orig, new, tofile=filepath))
+    if diff_lines:
+        diff_lines = [
+                line if line.endswith("\n") else line + "\n" for line in diff_lines[:-1]
+        ] + [diff_lines[-1]]
+    return "".join(diff_lines)
+
+
+def run(filepath, dry):
+    original = ''
+    fixed = ''
+
+    with open(filepath, 'r') as file:
+        original = file.readlines()
+        fixed = fix(original, filepath)
+
+    if not dry:
+        with open(filepath, 'w') as file:
+            for line in fixed:
+                file.write(line)
+
+    delta = diff(original, fixed, filepath)
+    sys.stdout.writelines(delta)
+
+
+def main(filepath, dry):
+    if os.path.isfile(filepath):
+        run(filepath, dry)
+    elif os.path.exists(filepath):
+        for path in glob.glob(filepath + '/**/*.scala.html', recursive=True):
+            run(path, dry)
+    else:
+        for path in glob.glob('./**/*.scala.html', recursive=True):
+            run(path, dry)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=textwrap.dedent('''\
+            Find and fix twirl templates that were messed up by
+            IntelliJ refactoring and code format. Run with '--dry'
+            flag first to see changes.
+            '''))
+    parser.add_argument(
+            'file',
+            nargs='?',
+            default=os.getcwd(),
+            help='file or directory, defaults to current working dir')
+    parser.add_argument(
+            '--dry',
+            action="store_true",
+            default=False,
+            help="do not write changes, only output diff")
+    args = parser.parse_args()
+
+    main(args.file, args.dry)
